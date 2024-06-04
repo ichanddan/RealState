4842f4f2052104dc87c2fbf9cb6f0686ce3ccf75
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-04 12:03:34 -0700

    Create NaveBar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Input,
+} from "@nextui-org/react";
+import { FaSearch } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <Input
+          placeholder="Serach here..."
+          endContent={
+            <FaSearch className="text-xl text-default-400 pointer-events-none" />
+          }
+          className="max-w-xs"
+        />
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
