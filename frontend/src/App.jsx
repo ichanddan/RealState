ac2666d7ace9b611ce2277334cd15c0f0f4f940c
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-05 21:11:13 +0530

    Setup React Route Dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import Signup from "./Pages/Signup";
+import Profile from "./Pages/Profile";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <>
-    <Header/>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
