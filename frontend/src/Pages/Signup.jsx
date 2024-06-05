2aa774b5aba1dcaddfa0e967d8d454b058cd07c1
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-05 22:36:05 +0530

    Complite Signup funcionality

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,9 +1,43 @@
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Signup() {
+  const [formData, setFormData] = useState([]);
+  const [errors, setErrors] = useState([]);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const Submit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/v1/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success===false){
+        toast.error("Signup faild", {
+            position: "top-right",
+        });
+      }else{
+        toast.success("Signup successfully", {
+            position: "top-right",
+        });
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
   return (
     <div className="flex items-center justify-center dark:bg-gray-800 p-6 md:p-10">
       <div className="max-w-md w-full space-y-6">
@@ -13,11 +47,18 @@ export default function Signup() {
         <form className="space-y-4">
           <div>
             <label>Name</label>
-            <Input id="name" type="text" placeholder="John Doe" required />
+            <Input
+              onChange={handleChange}
+              id="name"
+              type="text"
+              placeholder="John Doe"
+              required
+            />
           </div>
           <div>
             <label>Email</label>
             <Input
+              onChange={handleChange}
               id="email"
               type="email"
               placeholder="example@email.com"
@@ -27,13 +68,14 @@ export default function Signup() {
           <div>
             <label>Password</label>
             <Input
+              onChange={handleChange}
               id="password"
               type="password"
               placeholder="Enter Your Password"
               required
             />
           </div>
-          <Button type="submit" className="w-full">
+          <Button onClick={Submit} type="submit" className="w-full">
             Sign Up
           </Button>
         </form>
@@ -48,6 +90,7 @@ export default function Signup() {
           </Link>
         </p>
       </div>
+      <ToastContainer />
     </div>
   );
 }
