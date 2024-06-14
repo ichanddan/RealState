6a748d7d64e29416c97897cfd1b40f9f19347b5d
chandan kumar maurya <code.us.cm@gmail.com>
2024-06-14 23:53:00 +0530

    add funcationality product list

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,71 +1,261 @@
-import React, { useState } from "react";
-import { Button, Input, Textarea } from "@nextui-org/react";
-import { Checkbox } from "@nextui-org/checkbox";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Product() {
-  const [isOfferChecked, setIsOfferChecked] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
+  });
+  console.log(formData);
 
-  const handleOfferChange = (e) => {
-    setIsOfferChecked(e.target.checked);
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData({
+        ...formData,
+        type: e.target.id,
+      });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.checked,
+      });
+    }
+
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/v1/listproduct", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      if (data.success === true) {
+        toast.success("Product list successfully", {
+          position: "top-right",
+        });
+        setTimeout(() => {
+          navigate("/profile");
+        }, 2000);
+      }
+      console.log(data)
+    } catch (error) {
+      toast.error("Product list faild", {
+        position: "top-right",
+      });
+    }
+  };
   return (
-    <div className="max-w-4xl mx-auto p-8">
-      <h2 className="text-2xl font-bold mb-10 text-center">Create a Listing</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-        <div className="space-y-4">
-          <Input type="text" label="Name" labelPlacement={"outside"} />
-          <Textarea
-            label="Description"
-            placeholder="Enter your description"
-            className="py-3"
-          />{" "}
-          <Input type="text" label="Address" labelPlacement={"outside"} />
-          <div className="flex items-center gap-1 space-x-4">
-            <Checkbox>Sell</Checkbox>
-            <Checkbox defaultSelected>Rent</Checkbox>
-            <Checkbox>Parking spot</Checkbox>
-            <Checkbox>Furnished</Checkbox>
-          </div>
-          <Checkbox checked={isOfferChecked} onChange={handleOfferChange}>
-            Offer
-          </Checkbox>
-          <div className="flex items-center space-x-4">
-            <Input type="number" label="Beds" labelPlacement={"outside"} />
-            <Input type="number" label="baths" labelPlacement={"outside"} />
-          </div>
-          <div className="flex items-center space-x-4">
-            <Input
-              type="number"
-              label="Regular price ($ / Month)"
-              labelPlacement={"outside"}
-              className="w-1/2"
-            />
-            {isOfferChecked && (
-              <Input
-                type="number"
-                label="Offer price ($ / Month)"
-                labelPlacement={"outside"}
-                className="w-1/2"
+    <main className="p-3 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col gap-4 flex-1">
+          <input
+            type="text"
+            placeholder="Name"
+            className="border p-3 rounded-lg"
+            id="name"
+            maxLength="62"
+            minLength="10"
+            required
+            onChange={handleChange}
+            value={formData.name}
+          />
+          <textarea
+            type="text"
+            placeholder="Description"
+            className="border p-3 rounded-lg"
+            id="description"
+            required
+            onChange={handleChange}
+            value={formData.description}
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            className="border p-3 rounded-lg"
+            id="address"
+            required
+            onChange={handleChange}
+            value={formData.address}
+          />
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type === "sale"}
               />
-            )}
-          </div>
-        </div>
-        <div>
-          <div className="mb-4">
-            <div className="flex items-center space-x-2">
-              <Input
-                type="number"
-                label="Enter Image Url"
-                labelPlacement={"outside"}
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type === "rent"}
               />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.parking}
+              />
+              <span>Parking spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.furnished}
+              />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.offer}
+              />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+                onChange={handleChange}
+                value={formData.bedrooms}
+              />
+              <p>Beds</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bathrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+                onChange={handleChange}
+                value={formData.bathrooms}
+              />
+              <p>Baths</p>
+            </div>
+            <div className="flex items-center justify-around gap-5 w-1/2">
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  id="regularPrice"
+                  min="50"
+                  max="10000000"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                  onChange={handleChange}
+                  value={formData.regularPrice}
+                />
+                <div className="flex flex-col items-center">
+                  <p>Regular price</p>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">($ / month)</span>
+                  )}
+                </div>
+              </div>
+              {formData.offer && (
+                <div className="flex items-center gap-2">
+                  <input
+                    type="number"
+                    id="discountPrice"
+                    min="0"
+                    max="10000000"
+                    required
+                    className="p-3 border border-gray-300 rounded-lg"
+                    onChange={handleChange}
+                    value={formData.discountPrice}
+                  />
+                  <div className="flex flex-col items-center">
+                    <p>Discounted price</p>
+
+                    {formData.type === "rent" && (
+                      <span className="text-xs">($ / month)</span>
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
-      </div>
-      <Button className="w-full justify-center bg-green-600 text-white">
-        CREATE LISTING
-      </Button>
-    </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="flex gap-4">
+            <input
+              placeholder="Image link"
+              onChange={handleChange}
+              className="p-3 border border-gray-300 rounded w-full"
+              id="images"
+            />
+          </div>
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create listing
+          </button>
+        </div>
+      </form>
+      <ToastContainer />
+    </main>
   );
 }
